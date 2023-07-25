5601a62c6e09b288a4d3f61fdae9c95ffaaa6007
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-07-25 17:09:39 -0300

    Create the form to register de date.

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path 
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('create_user/', views.create_user, name="create_user"),
+    path('edit_user/<int:id>', views.edit_user, name="edit_user"),
+    path('delete_user/<int:id>', views.delete_user, name="delete_user")
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import User
 
 # Create your views here.
 def home(request):
-    title = 'Home'
-    return render(request, 'home/index.html', {'title': title})
+    usuarios = User.objects.all()
+    return render(request, 'home/index.html', {'usuarios': usuarios})
+
+
+def create_user(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cidade = request.POST.get('cidade')
+        User.objects.create(nome=nome, cidade=cidade)
+        usuarios = User.objects.all()
+    return render(request, 'home/index.html', {'usuarios': usuarios})
+
+
+def edit_user(request, id):
+    usuario = get_object_or_404(User, id=id)
+    if request.method == 'POST':
+        usuario.nome = request.POST.get('nome')
+        usuario.cidade = request.POST.get('cidade')
+        usuario.save()
+        return redirect('home')
+    return render(request, 'home/update.html', {'usuario': usuario})
+
+
+def delete_user(request, id):
+    usuario = get_object_or_404(User, id=id)
+    usuario.delete()
+    return redirect('home')
